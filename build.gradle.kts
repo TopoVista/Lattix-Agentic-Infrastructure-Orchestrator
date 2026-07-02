cfd9eaaa65b59aeb5a57d670611dedf44cc3b55a
souradeep <souradeepghosh608@gmail.com>
2026-07-02 21:56:28 +0530

    folder structure v1

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    java
+}
+
+allprojects {
+    group = "com.lattix"
+    version = "0.1.0"
+
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "lattix"
+
+// Add Java modules as they are implemented, for example:
+// include("gateway:api-gateway")
+// include("services:workspace-service")
